b78d81c95bd70f72533cc0767bb77fc3075497a0
suciamaa <109338761+suciamaa@users.noreply.github.com>
2024-01-04 21:51:59 +0700

    new stylee

--- js/script.js
+++ js/script.js
@@ -27,13 +27,19 @@ document.addEventListener('DOMContentLoaded', function(){
 function hitungluasSegitiga(){
     alas1 = document.getElementById("alas1").value;
     tinggi1 = document.getElementById("tinggi1").value;
+    if (alas1 == 0 || tinggi1 == 0){
+        window.alert("Masukkan nilai yang valid");
+        return;
+    }
     luas1 = 1/2*alas1*tinggi1;
-
-    
     document.getElementById("luas1").innerText = "Luas = 1/2 x a  x  t " + "\nLuas = 1/2 x " + alas1 + " x " + tinggi1 + "\n Luas = " + luas1 ;
 }
 
 function resetluasSegitiga(){
+    if (document.getElementById("luas1").innerText == 0){
+        window.alert("Lakukan perhitungan terlebih dahulu");
+        return;
+    }
     document.getElementById("alas1").value = "";
     document.getElementById("tinggi1").value = "";
     document.getElementById("luas1").innerText = "";
@@ -43,13 +49,19 @@ function hitungkelilingSegitiga(){
     s1 = parseFloat(document.getElementById("s1").value);
     s2 = parseFloat(document.getElementById("s2").value);
     s3 = parseFloat(document.getElementById("s3").value);
+    if (alas1 == 0 || tinggi1 == 0){
+        window.alert("Masukkan nilai yang valid");
+        return;
+    }
     keliling = s1 + s2 + s3;
-
-    
     document.getElementById("keliling").innerText = "Keliling = S1 + S2 + S3 " + "\nKeliling = " + s1 + " + " + s2 + " + " + s3 + "\n Keliling = " + keliling ;
 }
 
 function resetkelilingSegitiga(){
+    if (document.getElementById("luas1").innerText == 0){
+        window.alert("Lakukan perhitungan terlebih dahulu");
+        return;
+    }
     document.getElementById("s1").value = "";
     document.getElementById("s2").value = "";
     document.getElementById("s3").value = "";
@@ -85,12 +97,19 @@ document.addEventListener('DOMContentLoaded', function(){
 function hitungluasJajargenjang(){
     alas2 = document.getElementById("alas2").value;
     tinggi2 = document.getElementById("tinggi2").value;
+    if (alas1 == 0 || tinggi1 == 0){
+        window.alert("Masukkan nilai yang valid");
+        return;
+    }
     luas2 = alas2*tinggi2;
-
     document.getElementById("luas2").innerText = "Luas = a  x  t  " + "\nLuas = " + alas2 + " x " + tinggi2 + "\nLuas = " + luas2;
 }
 
 function resetluasJajargenjang(){
+    if (document.getElementById("luas1").innerText == 0){
+        window.alert("Lakukan perhitungan terlebih dahulu");
+        return;
+    }
     alas2 = document.getElementById("alas2").value = "";
     tinggi2 = document.getElementById("tinggi2").value = "";
     luas2 = document.getElementById("luas2").innerText = "";
@@ -99,12 +118,19 @@ function resetluasJajargenjang(){
 function hitungKelilingJajargenjang(){
     a = parseFloat(document.getElementById("a").value);
     b = parseFloat(document.getElementById("b").value);
+    if (alas1 == 0 || tinggi1 == 0){
+        window.alert("Masukkan nilai yang valid");
+        return;
+    }
     keliling2 = a + a + b + b;
-
     document.getElementById("keliling2").innerText = "Keliling = a + a + b + b " + "\nKeliling = 2a + 2b " + "\nKeliling = " + "2 x " + a + " +" + " 2 x " + b + "\nKeliling = " + keliling2; 
 }
 
 function resetKelilingJajargenjang(){
+    if (document.getElementById("luas1").innerText == 0){
+        window.alert("Lakukan perhitungan terlebih dahulu");
+        return;
+    }
     a = document.getElementById("a").value = "";
     b = document.getElementById("b").value = "";
     keliling2 = document.getElementById("keliling2").innerText = "";
